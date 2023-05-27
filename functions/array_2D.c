f7352632ac41b579cc0b0b8a71c411446a86b179
BigChaoor <114349415+BigChaoor@users.noreply.github.com>
2023-05-28 00:44:18 +0800

    Rename array_2D.cpp to array_2D.c

--- functions/array_2D.c
+++ functions/array_2D.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
-tic-tac-toe��Ϸ
-1 ����һ��3*3���󣬾����е�����Ϊ1��ʾ
-��λ���ϸ���һ��X��Ϊ0��ʾΪO
-2 �����жϸþ������Ƿ��л�ʤ��һ�������
-��ʾ��ʤһ�����ַ�X��O����������˻�ʤ 
+tic-tac-toe游戏
+1 读入一个3*3矩阵，矩阵中的数字为1表示
+该位置上个有一个X，为0表示为O
+2 程序判断该矩阵中是否有获胜的一方，输出
+表示获胜一方的字符X或O，或输出无人获胜 
 **/
 
 int main(void){
@@ -16,14 +16,14 @@ int main(void){
  int numofO;
  int result = -1;
  
- // �������
+ // 读入矩阵
  for ( i=0; i<size; i++ ){
  	for ( j=0; j<size; j++ ){
  		scanf("%d", &board[i][j]);
 	 }
  } 
  
- // �����
+ // 检查行
  for ( i=0; i<size && result == -1; i++ ){
  	numofO = numofX = 0;
  	for ( j=0; j<size; j++ ){
@@ -39,7 +39,7 @@ int main(void){
 	 }
  } 
  
-// ����У��ͼ���еĴ���ϲ���board[j][i]�� 
+// 检查列（和检查行的代码合并？board[j][i]） 
 if ( result == -1 ) {
  for ( j=0; j<size && result == -1; j++ ){
  	numofO = numofX = 0;
@@ -57,7 +57,7 @@ if ( result == -1 ) {
  }
 }
 
-// ������Խ���
+// 检查正对角线
 numofO = numofX = 0;
 for ( i=0; i<size; i++ ){
 	if ( board[i][j] == 1 ){
@@ -71,7 +71,7 @@ if ( numofO == size ){
 } else if (numofX == size){
 	result = 1;
 }
-// ��鷴�Խ���
+// 检查反对角线
 numofO = numofX = 0;
 for ( i=0; i<size; i++ ){
 	if (board[i][size-i-1] == 1){
@@ -86,7 +86,7 @@ if ( result == 1 ){
 } else if (result == 0){
 	printf("O\n");
 } else {
-	printf("���˻�ʤ"); 
+	printf("无人获胜"); 
 }
 
 return 0;
